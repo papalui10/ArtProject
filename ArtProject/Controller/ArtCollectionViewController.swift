9ba44148a0c6d0bdbe879dc8ceb8238fea39b582
Luis Veliz <luisvelizjr@gmail.com>
2019-05-09 13:22:47 -0600

    Created ArtPanel and a protected paintComopnent method.

--- ArtProject/Controller/ArtCollectionViewController.swift
+++ ArtProject/Controller/ArtCollectionViewController.swift
@@ -102,16 +102,35 @@ public class ArtCollectionViewController: UICollectionViewController
     // MARK: UICollectionViewDelegate
 
    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath)  -> CGSize
+   {
+    let paddingSpace = sectionInsets.left * (itemsPerRowCompact + 1)
+    let availableWidth = view.frame.width - paddingSpace
+    let widthPerItem = availableWidth / itemsPerRowCompact
     
-    {
-        let paddingSpace = sectionInsets.left * (itemsPerRowCompact + 1)
-        let availableWidth = view.frame.width - paddingSpace
-        let widthPerItem = availableWidth / itemsPerRowCompact
-        
-        return CGSize(width: widthPerItem, height: widthPerItem)
+    return CGSize(width: widthPerItem, height: widthPerItem)
     }
     
+    public override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        let imageView = UIImageView(image: creativeCS[indexPath.row])
+        imageView.frame = self.view.frame
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
     
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        imageView.addGestureRecognizer(tap)
+    
+        self.view.addSubview(imageView)
+    }
+    
+    // Use to go back from full mode
+    @objc
+    private func dismissFullscreenImage(_ sender: UITapGestureRecognizer)
+    {
+        sender.view?.removeFromSuperview()
+    }
+
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets
     {
         return sectionInsets
